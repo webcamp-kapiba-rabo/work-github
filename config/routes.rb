0f6969cf6adc97db16ef7445a7e61ecea43e68c6
yutatanomoki <yuta3071@gmail.com>
2021-01-23 04:07:44 +0000

    [add]ImageMagick導入

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  devise_for :admins
+  devise_for :admins, controllers: {
+  sessions: 'admins/sessions'
+  }
   
   namespace :admin do
     resources :customers,only: [:index,:show,:edit,:update] 
