62b73958224c35aeb6473442cd27e5d7d1261a22
yutatanomoki <yuta3071@gmail.com>
2021-01-24 02:59:22 +0000

    [fixed]devise

--- Gemfile
+++ Gemfile
@@ -55,5 +55,4 @@ gem 'font-awesome-sass', '~> 5.13'
 
 gem 'kaminari','~> 1.2.1'
 
-
 gem 'bootsnap', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -10,15 +10,16 @@ Rails.application.routes.draw do
     resources :customers,only: [:index,:show,:edit,:update] 
     resources :products, only: [:new, :create, :index, :show, :edit, :update]
     root 'products#top'
-    resources :orders, only: [:new, :create, :index, :show]
+    resources :orders, only: [:update, :index, :show]
     resources :genres, only: [:create, :index, :edit, :update]
     resources :order_products, only: [:update]
   end
   
   devise_for :customers
+  
   scope module: :public do
     root 'homes#top'
-    get 'homes/about' => 'homes#about'
+    get 'homes/about' => 'homes#about'  
     get 'customer/quit' => 'customers#quit'
     patch 'customer/goodbye' => 'customers#goodbye'
     resource :customer, only: [:show, :edit, :update]
