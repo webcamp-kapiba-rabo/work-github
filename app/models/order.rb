6f71c1f91bd2041ac1d98269b69d2ea4de6ca4df
sato-tkm <t.28pnorudo@gmail.com>
2021-01-25 06:43:26 +0000

    update_order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
     belongs_to :customer
     has_many :order_products, dependent: :destroy
+    enum payment_method: { クレジットカード: false, 銀行振り込み: true}
+    enum order_status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4}
 end
