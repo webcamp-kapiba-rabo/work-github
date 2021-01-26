3c9d1839cac5951115ea08804bfebd113337afc6
mapi8808 <mapi.008.k@gmail.com>
2021-01-26 14:15:17 +0000

    admin側のgenres、productsを修正しました。

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,4 @@
 class Genre < ApplicationRecord
     has_many :products, dependent: :destroy
+    validates :name, length:  { in: 1..20 }, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,15 @@ class Product < ApplicationRecord
     has_many :cart_products, dependent: :destroy
     has_many :order_products, dependent: :destroy
     attachment :image
-    
+
+    validates :name, length:  { in: 1..20 }, presence: true
+    validates :body, length: { maximum: 50 }, presence: true
+    validates :excluding_tax_price, length: { in: 1..7 }, presence: true
+    validates :sale_status, inclusion: { in: [true, false] }
+
+
     def including_tax_price
         (self.excluding_tax_price * 1.1).floor
     end
-    
+
 end
