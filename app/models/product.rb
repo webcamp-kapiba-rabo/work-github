c19ac0d3517a3a83d0898d506da1699c3bd2c42a
mapi8808 <mapi.008.k@gmail.com>
2021-01-26 09:53:00 +0000

    genres、products修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
     belongs_to :genre
     has_many :cart_products, dependent: :destroy
     has_many :order_products, dependent: :destroy
-    attachment :image_id
+    attachment :image
     
     def including_tax_price
         (self.excluding_tax_price * 1.1).floor
