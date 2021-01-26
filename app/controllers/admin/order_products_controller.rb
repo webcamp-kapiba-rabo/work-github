130e29c2e43ded195c9cb1db3133a6aa9db074cc
yutatanomoki <yuta3071@gmail.com>
2021-01-26 02:40:07 +0000

    [fixed]genreコントローラー修正

---
+++ app/controllers/admin/order_products_controller.rb
@@ -0,0 +1,24 @@
+class Admin::OrderProductsController < ApplicationController
+
+
+
+
+  before_action :authenticate_admin!
+
+      def update
+        @order_product = OrderProduct.find(params[:id])
+        if @order_product.update(order_product_params)
+            flash[:success] = "制作ステータスを変更しました"
+    	    redirect_to admin_order_path(@order_product.order)
+    	else
+    	    render "show"
+    	end
+      end
+
+  private
+  
+  def order_product_params
+		  params.require(:order_product).permit(:sale_status)
+  end
+
+end
