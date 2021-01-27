e41f97c83bfe6d416e3b625fe43bf68eb9d12c3d
e-last <statw54@gmail.com>
2021-01-27 04:59:49 +0000

    cart_product修正

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -9,25 +9,32 @@ class Public::CartProductsController < ApplicationController
   def create
      @cart_product = CartProduct.new(cart_products_params)
     @cart_product.customer_id = @customer.id
-    @cart_product.save
-    redirect_to cart_products_path
+    
+    @update_cart_product =  CartProduct.find_by(product: @cart_product.product)
+    if @update_cart_product.present? && @cart_product.valid?
+        @cart_product.amount += @update_cart_product.amount
+        @update_cart_product.destroy
+    end
+    
+      @cart_product.save
+      redirect_to cart_products_path
   end
   
   def update
-    @cart_products = CartProduct.where(customer_id: @customer.id)
-    @cart_products.update(cart_products_params)
+   @cart_product = CartProduct.find( params[:id])
+    @cart_product.update(cart_products_params)
     redirect_back(fallback_location: root_path)
   end
   
   def destroy
-    @cart_product = CartProduct.find(customer_id: @customer.id, product_id: params[:product_id])
+    @cart_product = CartProduct.find( params[:id])
     @cart_product.destroy
     redirect_back(fallback_location: root_path)
   end
   
   def all_destroy
     @cart_products = CartProduct.where(customer_id: @customer.id)
-    @cart_products.destroy
+    @cart_products.destroy_all
     redirect_back(fallback_location: root_path)
   end
   
