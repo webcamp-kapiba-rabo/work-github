af986f2e6b3ed25d42eabd255d7b4c21d47d651c
e-last <statw54@gmail.com>
2021-01-27 08:49:15 +0000

    admin/customerのupdate修正

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -16,8 +16,13 @@ class Admin::CustomersController < ApplicationController
   
   def update
     @customer = Customer.find(params[:id])
-    @Customer.update(user_params)
+    @customer.update(customer_params)
     redirect_to admin_customer_path(@customer.id)
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email, :membership_status)
   end
   
 end
